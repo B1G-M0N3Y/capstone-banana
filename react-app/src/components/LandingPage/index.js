94d0b8bfa6b85f69935cc8c8f35c1e3e834ae15e
B1G-M0N3Y <noahkerner843@gmail.com>
2022-11-26 18:32:51 -0700

    finished banana section of bottom component

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -24,6 +24,31 @@ const LandingPage = () => {
           <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Banana.png/800px-Banana.png'></img>
         </div>
       </div>
+      <div className="bottom-container">
+        <div className="bottom bananas">
+          <div className="bottom-text">
+            <h1 className="bottom-title">Bananas</h1>
+            <h3 className="bottom-desc">Bigger. Bolder. Bananer.</h3>
+            <div className="bottom-container-links">
+              <NavLink className='navlink' to="/">Learn More ></NavLink>
+              <NavLink className='navlink' to="/">Order Now ></NavLink>
+            </div>
+          </div>
+          <img src="https://i.imgur.com/MecVzxL.png"></img>
+        </div>
+        <div className="bottom banana-bunches">
+          <h1 className="bottom-title">Banana Bunches</h1>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Banana.png/800px-Banana.png'></img>
+        </div>
+        <div className="bottom banana-peels">
+          <h1 className="bottom-title">Banana Peels</h1>
+          <img src='https://i.imgur.com/FnT0Ovs.png'></img>
+        </div>
+        <div className="bottom bananos">
+          <h1 className="bottom-title">Bananos</h1>
+          <img src='https://i.imgur.com/le3ZptD.png'></img>
+        </div>
+      </div>
     </>
   );
 };
