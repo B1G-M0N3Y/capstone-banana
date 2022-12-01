d6dbc8e3e020e4f213837348c6522a30fd4a94c0
B1G-M0N3Y <noahkerner843@gmail.com>
2022-12-01 09:48:50 -0700

    basic layout on single product pages

--- react-app/src/components/ProductPurchasePage/SingleItemPage.js
+++ react-app/src/components/ProductPurchasePage/SingleItemPage.js
@@ -1,20 +1,25 @@
 import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
+import { useCart } from "../../context/CartContext"
 import './SingleItemPage.css'
 
 const SingleItemPage = () => {
   const { itemId } = useParams(':itemId')
   const [item, setItem] = useState({})
-
   const [activeImage, setActiveImage] = useState(0)
+  const {cart, setCart} = useCart()
+  const currentUser = useSelector(state => state.session.user)
 
   const updateImage = (newImage) => {
+    console.log('activeImage', activeImage)
     if (newImage < 0) {
       newImage = 0
     } else if (newImage >= item.images.length) {
       newImage = item.images.length - 1;
     }
 
+    console.log('newImage', newImage)
     setActiveImage(newImage)
   }
 
@@ -28,39 +33,83 @@ const SingleItemPage = () => {
     fetchItem()
   }, [])
 
+  const makeCart = (banan) => {
+    let increase = false
+    const newCart = []
+
+    for (const item of cart) {
+      if (item?.id === banan.id) {
+        newCart.push({
+          id: item.id,
+          quantity: ++item.quantity
+        })
+        increase = true
+      } else {
+        newCart.push(item)
+      }
+    }
+
+    if (!increase) {
+      return [...newCart, banan]
+    }
+    return newCart
+  }
+
+  const addToCart = (banan) => {
+    const cartItem = {
+      id: banan.id,
+      quantity: 1
+    }
+
+    const newCart = makeCart(cartItem)
+    setCart(newCart)
+
+    if (currentUser) localStorage.setItem(currentUser.email, JSON.stringify(cart));
+    else localStorage.setItem('default', JSON.stringify(cart))
+  }
+
+  console.log(item.images)
+
   return (
-    <div>
+    <div className="single-item-page">
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <h1>{`Buy ${item.name}`}</h1>
       <h4>Purchase now for {item.price}</h4>
       <div className="product-window">
-        <button
-          className='carousel-button prev'
-          onClick={() => {
-            updateImage(activeImage - 1);
-          }}
-        >
-          <i class="fa-solid fa-chevron-left"></i>
-        </button>
-        {/* carousel */}
         <div className="product-image-carousel">
-          {/* inner */}
-          <div className="product-image-inner" style={{ transform: `translateX(-${activeImage * 100}%)` }}>
+          <button
+            className='product-page-carousel-prev'
+            onClick={() => {
+              updateImage(activeImage - 1);
+            }}
+          >
+            <i class="fa-solid fa-chevron-left"></i>
+          </button>
+          <div className="product-image-inner" style={{ transform: `translateX(-${activeImage * 50}%)` }}>
             {item.images?.map(image => (
               <img src={image.image_url}></img>
             ))}
           </div>
+          <button
+            className='product-page-carousel-next'
+            onClick={() => {
+              updateImage(activeImage + 1);
+            }}
+          >
+            <i class="fa-solid fa-chevron-right"></i>
+          </button>
         </div>
-        <button
-              className='carousel-button next'
-              onClick={() => {
-                updateImage(activeImage + 1);
-              }}
-            >
-              <i class="fa-solid fa-chevron-right"></i>
-            </button>
-        <button>
-          Click here to Buy
-        </button>
+        <div className='purchase-container'>
+          <p>Price: </p>
+          <h1>${item.price}</h1>
+          <button onClick={() => addToCart(item)}>
+            Add to Cart
+          </button>
+        </div>
       </div>
     </div>
   )
