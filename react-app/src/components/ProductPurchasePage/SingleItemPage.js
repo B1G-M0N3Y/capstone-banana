7037721825195398e9c1bf3b10476da2349a8312
B1G-M0N3Y <noahkerner843@gmail.com>
2022-11-30 23:39:49 -0700

    added buttons on image carousel

--- react-app/src/components/ProductPurchasePage/SingleItemPage.js
+++ react-app/src/components/ProductPurchasePage/SingleItemPage.js
@@ -1,9 +1,23 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import './SingleItemPage.css'
 
 const SingleItemPage = () => {
   const { itemId } = useParams(':itemId')
   const [item, setItem] = useState({})
+
+  const [activeImage, setActiveImage] = useState(0)
+
+  const updateImage = (newImage) => {
+    if (newImage < 0) {
+      newImage = 0
+    } else if (newImage >= item.images.length) {
+      newImage = item.images.length - 1;
+    }
+
+    setActiveImage(newImage)
+  }
+
 
   useEffect(() => {
     async function fetchItem() {
@@ -17,16 +31,36 @@ const SingleItemPage = () => {
   return (
     <div>
       <h1>{`Buy ${item.name}`}</h1>
-      <div>
+      <h4>Purchase now for {item.price}</h4>
+      <div className="product-window">
+        <button
+          className='carousel-button prev'
+          onClick={() => {
+            updateImage(activeImage - 1);
+          }}
+        >
+          <i class="fa-solid fa-chevron-left"></i>
+        </button>
         {/* carousel */}
-        <div>
+        <div className="product-image-carousel">
           {/* inner */}
-          <div>
+          <div className="product-image-inner" style={{ transform: `translateX(-${activeImage * 100}%)` }}>
             {item.images?.map(image => (
               <img src={image.image_url}></img>
             ))}
           </div>
         </div>
+        <button
+              className='carousel-button next'
+              onClick={() => {
+                updateImage(activeImage + 1);
+              }}
+            >
+              <i class="fa-solid fa-chevron-right"></i>
+            </button>
+        <button>
+          Click here to Buy
+        </button>
       </div>
     </div>
   )
