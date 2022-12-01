8e2ab9851918cb436efb4c0b7fed841b5874091e
B1G-M0N3Y <noahkerner843@gmail.com>
2022-12-01 11:49:41 -0700

    added validation error to review section

--- react-app/src/components/reviews/CreateReview.js
+++ react-app/src/components/reviews/CreateReview.js
@@ -1,18 +1,37 @@
 import { useState } from "react"
+import { useSelector } from "react-redux"
 
 const CreateReview = () => {
-  const [reviewBody, setReviewBody] = useState([])
+  const [reviewBody, setReviewBody] = useState('')
+  const [validationErrors, setValidationErrors] = useState([])
+  const currentUser = useSelector(state => state.session.currentUser)
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    const errors = []
+
+
+    if(!reviewBody) errors.push('You must write a review to submit one')
+
+
+    setValidationErrors(errors)
+  }
 
   return (
     <div className="create-review">
       <h4>Got anything to say? HUH PUNK?</h4>
       <p>Leave your review.</p>
-      <form>
+      <form onSubmit={handleSubmit}>
+        {validationErrors.length > 0 &&
+          validationErrors.map(error => <p>{error}</p>)}
         <textarea
           className="enter-review"
           value={reviewBody}
           onChange={(e)=>setReviewBody(e.target.value)}
         ></textarea>
+        <button type="submit">
+          Add Review
+        </button>
       </form>
     </div>
   )
