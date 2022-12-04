ddf81e252353ffa6382b21ec15ad770d3d3a5a9f
B1G-M0N3Y <noahkerner843@gmail.com>
2022-12-03 18:11:13 -0700

    began basic implementation for cart total

--- react-app/src/components/Checkout/index.js
+++ react-app/src/components/Checkout/index.js
@@ -1,11 +1,37 @@
 import { useSelector } from "react-redux"
 import CheckoutItem from "./CheckoutItem"
 import './Checkout.css'
+import { useEffect, useState } from "react"
 
 const Checkout = () => {
   const currentUser = useSelector(state => state.session.user)
-
+  const [allItems, setAllItems] = useState([]);
   const cart = JSON.parse(localStorage.getItem(currentUser?.email || 'default'))
+
+  useEffect(()=> {
+    async function fetchData() {
+      // TODO: CONVERT BACK TO FETCHING SINGLE BANANA
+      const apibanana = await fetch(`/api/items`)
+
+      setAllItems(await apibanana.json())
+    }
+    fetchData()
+    getTotal()
+  },[])
+
+  const getTotal = () => {
+    let total = 0;
+    console.log(cart)
+
+    for (const item of cart){
+      const price = allItems.filter(allItem => allItem.id === item.id)[0].price
+      console.log(price)
+      console.log(item.quantity)
+      total = total += (price * item.quantity)
+    }
+
+    return total;
+  }
 
   console.log(cart)
 
@@ -20,6 +46,7 @@ const Checkout = () => {
           </>
         ))}
       </div>
+      <p>{getTotal()}</p>
     </div>
   )
 }
