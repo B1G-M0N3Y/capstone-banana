1cd5ae55390e72702a13358c5b2f3e874322b231
B1G-M0N3Y <noahkerner843@gmail.com>
2022-12-05 02:54:35 -0700

    fixed read error on delete in shopping cart

--- react-app/src/components/NavBar/ShoppingCartNav.js
+++ react-app/src/components/NavBar/ShoppingCartNav.js
@@ -3,30 +3,36 @@ import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import { useCart } from '../../context/CartContext'
 
-const ShoppingCartItem = ({ item }) => {
+const ShoppingCartItem = ({ item, cart, setCart }) => {
   const [quantity, setQuantity] = useState(item?.quantity)
   const [itemDetails, setItemDetails] = useState({})
-  const { cart, setCart } = useCart()
   const currentUser = useSelector(state => state.session.user)
-
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch(`/api/items/${item.id}`)
-      const responseData = await response.json();
-      setItemDetails(responseData)
-    }
-    fetchData()
-  }, [])
 
   useEffect(() => {
     if (item) setQuantity(item.quantity)
   }, [cart])
 
   const deleteItem = (itemId) => {
-    const newCart = cart.filter(item => item?.id !== itemId)
+    console.log('when clicked', itemId)
+    console.log(itemId)
+    const newCart = cart.filter(item => {
+      console.log('deleted id', itemId)
+      console.log('loop id', item)
+      return item?.id !== itemDetails.id
+    })
     setCart(newCart)
     localStorage.setItem((currentUser?.email || 'default'), JSON.stringify(newCart))
   }
+
+  useEffect(() => {
+    async function fetchData() {
+      console.log(item.id)
+      const response = await fetch(`/api/items/${item.id}`)
+      const responseData = await response.json();
+      setItemDetails(responseData)
+    }
+    fetchData()
+  }, [deleteItem])
 
   const editCartItem = (quantity) => {
     setQuantity(quantity)
@@ -76,7 +82,7 @@ const ShoppingCartItem = ({ item }) => {
             </div>
           </div>
         </div>
-        <i class="fa-solid fa-trash" onClick={() => deleteItem(item?.id)}></i>
+        <i class="fa-solid fa-trash" onClick={() => deleteItem(item.id)}></i>
       </div >
       <hr></hr>
     </>
@@ -93,9 +99,9 @@ const ShoppingCartNav = () => {
         <i class="fa-solid fa-bag-shopping " onClick={() => setShowCart(!showCart)}></i>
         {showCart &&
           <div className='cart-dropdown'>
-            {cart?.map(item => (
-              <ShoppingCartItem item={item} />
-            ))}
+            {cart?.map(item => {
+              return <ShoppingCartItem item={item} cart={cart} setCart={setCart}/>
+            })}
             <NavLink to="/cart/current">
               <button id='checkout-nav'>Checkout</button>
             </NavLink>
