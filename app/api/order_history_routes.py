d139ea5c1cd6ae4c7e44be860e29cb3a04569a42
B1G-M0N3Y <noahkerner843@gmail.com>
2023-02-01 14:33:13 -0600

    added backend add order history route

--- app/api/order_history_routes.py
+++ app/api/order_history_routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from ..forms.order_form import OrderForm
 from app.models.order_history import db, Order_History
 from flask_login import login_required, current_user
 order_routes = Blueprint('orders', __name__)
@@ -23,3 +24,22 @@ def delete_from_order_history(order_id):
     db.session.delete(order)
     db.session.commit()
     return {"message": ["Order deleted."]}, 200
+
+@order_routes.route('', methods=['POST'])
+@login_required
+def add_to_order_history():
+    form = OrderForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        data = form.data
+        new_order = Order_History(
+            user_id = current_user.id,
+            item_id = data['item_id'],
+            quantity = data['quantity']
+        )
+
+        db.session.add(new_order)
+        db.session.commit()
+
+        return current_user.to_dict_order_history()
