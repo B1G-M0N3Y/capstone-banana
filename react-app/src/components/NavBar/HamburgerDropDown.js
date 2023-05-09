ef99dc338c3ee353ba8b9ee872f3195e5de6b759
B1G-M0N3Y <noahkerner843@gmail.com>
2023-05-09 15:31:39 -0500

    formatted hamburger drop down

--- react-app/src/components/NavBar/HamburgerDropDown.js
+++ react-app/src/components/NavBar/HamburgerDropDown.js
@@ -8,41 +8,69 @@ const HamburgerDropDown = () => {
   const currentUser = useSelector(state => state.session.user)
 
   return (
-    <div>
+    <div className="drop-down-container">
       <li>
-        <NavLink className='navlink' to='/items' exact={true} activeClassName='active'>
+        <NavLink
+          className='navlink drop-down-link'
+          to='/items'
+          exact={true}
+          activeClassName='active'>
           Store
         </NavLink>
       </li>
       <li>
-        <NavLink className='navlink' to='/items/1' exact={true} activeClassName='active'>
+        <NavLink
+          className='navlink drop-down-link'
+          to='/items/1'
+          exact={true}
+          activeClassName='active'>
           Banana
         </NavLink>
       </li>
       <li>
-        <NavLink className='navlink' to='/items/2' exact={true} activeClassName='active'>
+        <NavLink
+          className='navlink drop-down-link'
+          to='/items/2'
+          exact={true}
+          activeClassName='active'>
           Banana Pro
         </NavLink>
       </li>
       <li>
-        <NavLink className='navlink' to='/items/3' exact={true} activeClassName='active'>
+        <NavLink
+          className='navlink drop-down-link'
+          to='/items/3'
+          exact={true}
+          activeClassName='active'>
           Banana Bunch
         </NavLink>
       </li>
       <li>
-        <NavLink className='navlink' to='/items/5' exact={true} activeClassName='active'>
+        <NavLink
+          className='navlink drop-down-link'
+          to='/items/5'
+          exact={true}
+          activeClassName='active'>
           Banano
         </NavLink>
       </li>
       {!currentUser &&
         <>
           <li>
-            <NavLink className='navlink' to='/login' exact={true} activeClassName='active'>
+            <NavLink
+              className='navlink drop-down-link'
+              to='/login'
+              exact={true}
+              activeClassName='active'>
               Login
             </NavLink>
           </li>
           <li>
-            <NavLink className='navlink' to='/sign-up' exact={true} activeClassName='active'>
+            <NavLink
+              className='navlink drop-down-link'
+              to='/sign-up'
+              exact={true}
+              activeClassName='active'>
               Sign Up
             </NavLink>
           </li>
@@ -50,12 +78,20 @@ const HamburgerDropDown = () => {
       {currentUser &&
         <>
           <li>
-            <NavLink className='navlink' to='/reviews/current' exact={true} activeClassName='active'>
+            <NavLink
+              className='navlink drop-down-link'
+              to='/reviews/current'
+              exact={true}
+              activeClassName='active'>
               Your Reviews
             </NavLink>
           </li>
           <li>
-            <NavLink className='navlink' to='/orders/current' exact={true} activeClassName='active'>
+            <NavLink
+              className='navlink drop-down-link'
+              to='/orders/current'
+              exact={true}
+              activeClassName='active'>
               Your Orders
             </NavLink>
           </li>
